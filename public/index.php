538764c83c7191f5a3122ea3670fb540e8f9952e
julien <julien.seixas@ynov.com>
2018-05-25 10:37:54 +0200

    filters internship offer
    + begin to create filter to internship offer

--- public/index.php
+++ public/index.php
@@ -17,7 +17,41 @@ require_once($_SESSION["RacineServ"] . '/src/php/lienbdd.php');
 </head>
 
 <body>
-    <table>
+
+    <div class="header">
+
+    </div>
+
+    <div class="filter">
+        <div class="filter__skill">
+            <input type="checkbox" name="filter__skill-1" value="PHP"></br>
+            <input type="checkbox" name="filter__skill-2" value="Ergonomie"></br>
+            <input type="checkbox" name="filter__skill-3" value="SEO"></br>
+            <input type="checkbox" name="filter__skill-4" value="Symphony"></br>
+            <input type="checkbox" name="filter__skill-5" value="Node.js"></br>
+        </div>
+        <div class="filter__type">
+            <input type="checkbox" name="filter__type-1" value="Stage">
+            <input type="checkbox" name="filter__type-2" value="Alternance">
+        </div>
+        <div class="filter__level">
+            <input type="checkbox" name="filter__level-1" value="B1">
+            <input type="checkbox" name="filter__level-2" value="B2">
+            <input type="checkbox" name="filter__level-3" value="B3">
+            <input type="checkbox" name="filter__level-4" value="M1">
+            <input type="checkbox" name="filter__level-5" value="M2">
+        </div>
+        <div class="filter_date">
+            <div data-role="rangeslider">
+                <label for="price-min">Price:</label>
+                <input type="range" name="price-min" id="price-min" value="200" min="0" max="1000">
+                <label for="price-max">Price:</label>
+                <input type="range" name="price-max" id="price-max" value="800" min="0" max="1000">
+            </div>
+        </div>
+    </div>
+
+    <table class="offreTable">
         <tbody>
             <tr>
                 <th>Offre</th>
