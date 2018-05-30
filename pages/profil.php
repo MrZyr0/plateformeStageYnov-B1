616ada65961a626c2c40775889590a9eccacec85
julien <julien.seixas@ynov.com>
2018-05-30 17:22:16 +0200

    need changes for img

--- pages/profil.php
+++ pages/profil.php
@@ -4,26 +4,26 @@ require_once($_SESSION["RacineServ"] . '/src/php/lienbdd.php');
 if (isset($_POST['email'])) {
     // Create the Transport
     $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-        ->setUsername('stageynov@gmail.com')
-        ->setPassword('F0rtnitevie');
+    ->setUsername('stageynov@gmail.com')
+    ->setPassword('F0rtnitevie');
 
     // Create the Mailer using your created Transport
     $mailer = new Swift_Mailer($transport);
 
     // Create a message
     $message = (new Swift_Message('Contactez nos étudiants'))
-        ->setFrom([$_POST["email"] => $_POST["nom"]])
-        ->setTo(["stageynov@gmail.com"])
-        ->setBody("De l'entreprise " . $_POST["entreprise"] . " : \n" . $_POST["message"] . "\nContactez l'entreprise au " . $_POST["tel"]);
+    ->setFrom([$_POST["email"] => $_POST["nom"]])
+    ->setTo(["stageynov@gmail.com"])
+    ->setBody("De l'entreprise " . $_POST["entreprise"] . " : \n" . $_POST["message"] . "\nContactez l'entreprise au " . $_POST["tel"]);
 
     // Send the message
     $result = $mailer->send($message);
 }
 
 $statement = $connection->prepare("
-    SELECT *
-    FROM osi_offer
-    WHERE id = $idProfil;
+SELECT *
+FROM osi_offer
+WHERE id = $idProfil;
 ");
 $statement->execute();
 $offre = $statement->fetchAll();
@@ -37,95 +37,87 @@ $offre = $statement->fetchAll();
     <title><?= $offre[0]['title'] ?></title>
 </head>
 <body>
-<h1><?= $offre[0]['title'] ?></h1>
-<div class="contact">
-    <div>Contactez les étudiants</div>
-    <form method="post" action="">
-        <input type="email" name="email" placeholder="E-mail" required>
-        <br/>
-        <input type="tel" name="tel" placeholder="Téléphone" required>
-        <br/>
-        <input type="text" name="nom" placeholder="NOM" required>
-        <br/>
-        <input type="text" name="entreprise" placeholder="Nom de l'entreprise" required>
-        <br/>
-        <textarea style="resize: none" name="message" rows="10" cols="50" placeholder="Votre message"
-                  required></textarea>
-        <br>
-        <input type="submit" value="Envoyer"/>
-    </form>
-</div>
+    <h1><?= $offre[0]['title'] ?></h1>
+    <div class="contact">
+        <div>Contactez les étudiants</div>
+        <form method="post" action="">
+            <input type="email" name="email" placeholder="E-mail" required>
+            <br/>
+            <input type="tel" name="tel" placeholder="Téléphone" required>
+            <br/>
+            <input type="text" name="nom" placeholder="NOM" required>
+            <br/>
+            <input type="text" name="entreprise" placeholder="Nom de l'entreprise" required>
+            <br/>
+            <textarea style="resize: none" name="message" rows="10" cols="50" placeholder="Votre message"
+            required></textarea>
+            <br>
+            <input type="submit" value="Envoyer"/>
+        </form>
+    </div>
 
-<div class="content">
-    <div class="description">
-        <h2>PROFIL DE NOS ETUDIANTS</h2>
-        <?php print $offre[0]['description'] ?>
+    <div class="content">
+        <div class="description">
+            <h2>PROFIL DE NOS ETUDIANTS</h2>
+            <?php print $offre[0]['description'] ?>
+        </div>
+        <div class="competences">
+            <h2>COMPETENCES</h2>
+            <?php
+            $statement = $connection->prepare("
+            SELECT s.* FROM `osi_skill` s JOIN osi_offer_skill os ON os.skill_id = s.id AND os.offer_id = $idProfil;
+            ");
+            $statement->execute();
+            $skills = $statement->fetchAll();
+            for ($i = 0; $i < count($skills); $i++) {
+                print $skills[$i]['title'] . " ";
+            }
+            ?>
+        </div>
     </div>
-    <div class="competences">
-        <h2>COMPETENCES</h2>
-        <?php
-        $statement = $connection->prepare("
-        SELECT s.* FROM `osi_skill` s JOIN osi_offer_skill os ON os.skill_id = s.id AND os.offer_id = $idProfil;
-        ");
-        $statement->execute();
-        $skills = $statement->fetchAll();
-        for ($i = 0; $i < count($skills); $i++) {
-            print $skills[$i]['title'] . " ";
-        }
-        ?>
-    </div>
-</div>
-<div class="profil">
-    <div clas="image">
-        <?php
-<<<<<<< HEAD
-        if ((stripos($offre[0]['class'], 'informatique')) !== false) {
-            print '<img src="/img/icons/logo-school/ingesup.png" height="70px" class="imgynov" alt="logo ynov informatique">';
-        }
-        elseif ((stripos($offre[0]['class'], 'aeronautique')) !== false){
-            print '<img src="/img/icons/logo-school/aeronautique.png" height="70px" class="imgynov" alt="logo ynov aeronautique">';
-=======
-        if (((stripos($offre[0]['class'], 'informatique')) !== false)or((stripos($offre[0]['class'], 'ingesup')) !== false)or((stripos($offre[0]['class'], 'ingésup')) !== false)) {
-            print '<img src="/img/logo/informatique.png" height="70px" class="imgynov" alt="logo ynov informatique">';
-        }
-        elseif (((stripos($offre[0]['class'], 'aeronautique')) !== false)or((stripos($offre[0]['class'], 'systeme')) !== false)or((stripos($offre[0]['class'], 'aéronautique')) !== false)){
-            print '<img src="/img/logo/aeronautique.png" height="70px" class="imgynov" alt="logo ynov aeronautique">';
-        }
-        elseif (((stripos($offre[0]['class'], 'business')) !== false)or((stripos($offre[0]['class'], 'digital')) !== false)){
-            print '<img src="/img/logo/digitalbusiness.png" height="70px" class="imgynov" alt="logo ynov digital business">';
-        }
-        elseif ((stripos($offre[0]['class'], 'audiovisuel')) !== false){
-            print '<img src="/img/logo/audiovisuel.png" height="70px" class="imgynov" alt="logo ynov audiovisuel">';
-        }
-        elseif (((stripos($offre[0]['class'], 'jeux video')) !== false)or((stripos($offre[0]['class'], 'jeux vidéo')) !== false)or ((stripos($offre[0]['class'], 'animation')) !== false)){
-            print '<img src="/img/logo/jeuxvideos.png" height="70px" class="imgynov" alt="logo ynov jeux vidéos">';
-        }
-        elseif (((stripos($offre[0]['class'], 'graphic')) !== false)or ((stripos($offre[0]['class'], 'web')) !== false) or ((stripos($offre[0]['class'], 'com')) !== false)or ((stripos($offre[0]['class'], 'design')) !== false)){
-            print '<img src="/img/logo/webcom.png" height="70px" class="imgynov" alt="logo ynov graphic design">';
->>>>>>> 858495a7183235e1f00f55d8e37efd3b25a325a5
-        }
-        ?>
-    </div>
-    <div class="class">
-        <h2>CLASSE</h2>
-        <?php print $offre[0]['class'] ?>
-    </div>
-    <div class="type">
-        <h2>TYPE</h2>
-        <?php print $offre[0]['type'] ?>
-    </div>
-    <div class="debut">
-        <h2>DEBUT</h2>
-        <?php print $offre[0]['from_date'] ?>
-    </div>
-    <div class="fin">
-        <h2>FIN</h2>
-        <?php print $offre[0]['to_date'] ?>
-    </div>
-    <div class="fin">
-        <h2>PERIODE</h2>
-        <?php print $offre[0]['period'] ?>
-    </div>
-</div>
-</body>
-</html>
+    <div class="profil">
+        <div clas="image">
+            <?php
+            if ((stripos($offre[0]['class'], 'informatique')) !== false)
+            {
+                print '<img src="/img/icons/logo-school/ingesup.png" height="70px" class="imgynov" alt="logo ynov informatique">';
+            }
+            elseif ((stripos($offre[0]['class'], 'aeronautique')) !== false)
+            {}
+                elseif (((stripos($offre[0]['class'], 'business')) !== false)or((stripos($offre[0]['class'], 'digital')) !== false)){
+                    print '<img src="/img/logo/digitalbusiness.png" height="70px" class="imgynov" alt="logo ynov digital business">';
+                }
+                elseif ((stripos($offre[0]['class'], 'audiovisuel')) !== false){
+                    print '<img src="/img/logo/audiovisuel.png" height="70px" class="imgynov" alt="logo ynov audiovisuel">';
+                }
+                elseif (((stripos($offre[0]['class'], 'jeux video')) !== false)or((stripos($offre[0]['class'], 'jeux vidéo')) !== false)or ((stripos($offre[0]['class'], 'animation')) !== false)){
+                    print '<img src="/img/logo/jeuxvideos.png" height="70px" class="imgynov" alt="logo ynov jeux vidéos">';
+                }
+                elseif (((stripos($offre[0]['class'], 'graphic')) !== false)or ((stripos($offre[0]['class'], 'web')) !== false) or ((stripos($offre[0]['class'], 'com')) !== false)or ((stripos($offre[0]['class'], 'design')) !== false)){
+                    print '<img src="/img/logo/webcom.png" height="70px" class="imgynov" alt="logo ynov graphic design">';
+                }
+                ?>
+            </div>
+            <div class="class">
+                <h2>CLASSE</h2>
+                <?php print $offre[0]['class'] ?>
+            </div>
+            <div class="type">
+                <h2>TYPE</h2>
+                <?php print $offre[0]['type'] ?>
+            </div>
+            <div class="debut">
+                <h2>DEBUT</h2>
+                <?php print $offre[0]['from_date'] ?>
+            </div>
+            <div class="fin">
+                <h2>FIN</h2>
+                <?php print $offre[0]['to_date'] ?>
+            </div>
+            <div class="fin">
+                <h2>PERIODE</h2>
+                <?php print $offre[0]['period'] ?>
+            </div>
+        </div>
+    </body>
+    </html>
