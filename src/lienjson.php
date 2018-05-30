7dc2b4c493f70fd4dfa2c05df0e2b8e66bfc5dac
Gianni Scantamburlo <gianni.scantamburlo@ynov.com>
2018-05-30 17:22:30 +0200

    json

--- src/lienjson.php
+++ src/lienjson.php
@@ -1,9 +1,6 @@
 <?php
 // Si les données json sont dans un fichier distant:
-$json_source = file_get_contents('../../config.json');
-
-// Si les données json sont dans une variable sans passer par un fichier distant:
-$json_source = '{"nom":"Adriana", "naissance":"1981-06-12"}';
+$json_source = file_get_contents('/../config.json');
 
 // Décode le JSON
 $json_data = json_decode($json_source);
