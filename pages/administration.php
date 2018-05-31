a500ed9fa93c300e01e8ca65ded2590061097369
Gianni Scantamburlo <gianni.scantamburlo@ynov.com>
2018-05-31 16:11:03 +0200

    repush

--- pages/administration.php
+++ pages/administration.php
@@ -1,4 +1,10 @@
 <?php
+require_once($_SESSION["RacineServ"] . '/src/php/lienbdd.php');
+
+$statement = $connection->prepare("
+    INSERT INTO `osi_offer` (`id`, `title`, `type`, `class`, `description`, `period`, `from_date`, `to_date`, `categorie`) VALUES (NULL, 'Développeur java', 'Stage', '3ème année Informatique', '## Description\r\n\r\nLes étudiants de première année cherchent un stage. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat fermentum gravida. In eleifend venenatis dui, ut congue nisi ullamcorper ut. Nunc gravida rhoncus volutpat. In pharetra maximus purus quis elementum. Sed commodo auctor metus quis semper. Pellentesque sagittis condimentum massa ut rhoncus. Pellentesque luctus dignissim velit, eu malesuada tortor tristique eget. Fusce eget tempus orci.\r\n\r\n## Compétences acquises\r\n\r\nLes étudiants ont réalisé un **projet transvesal**. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat fermentum gravida. In eleifend venenatis dui, ut congue nisi ullamcorper ut. Nunc gravida rhoncus volutpat. In pharetra maximus purus quis elementum. Sed commodo auctor metus quis semper. Pellentesque sagittis condimentum massa ut rhoncus. Pellentesque luctus dignissim velit, eu malesuada tortor tristique eget. Fusce eget tempus orci.', '5 mois', '2018-05-25', '2018-10-18', 'informatique')
+");
+$statement->execute();
 ?>
 <!DOCTYPE html>
 <html lang="en">
