75ecffc28caaef0c048e3b74f593b59f25b84bb8
Gianni Scantamburlo <gianni.scantamburlo@ynov.com>
2018-06-02 00:01:46 +0200

    fin

--- pages/administration.php
+++ pages/administration.php
@@ -21,36 +21,53 @@ if (isset($_POST['title'])) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="/css/administration.css">
     <title>Administration</title>
 </head>
 <body>
-
-<form method="post" action="">
-    <div>AJOUTER UNE OFFRE:</div>
-    <input type="number" name="id" placeholder="id">
-    <br/>
-    <input type="text" name="title" placeholder="Titre" required>
-    <br/>
-    <input type="text" name="type" placeholder="Type" required>
-    <br/>
-    <input type="text" name="class" placeholder="Classe" required>
-    <br/>
-    <input type="text" name="period" placeholder="Période" required>
-    <br/>
-    <input type="date" name="from_date" placeholder="Du" required>
-    <br/>
-    <input type="date" name="to_date" placeholder="Au" required>
-    <br/>
-    <input type="text" name="categorie" placeholder="Catégorie" required>
-    <br/>
-    <textarea style="resize: none" name="description" rows="10" cols="50" placeholder="Description de l'offre:" required>
+<nav class="nav">
+    <a href="/"><img src="/img/icons/prj_ynov.png" alt="Logo du Campus Ynov de Lyon" class="nav__logo"></a>
+    <div class="nav__menu">
+        <a href="http://www.ynovlyon.com/fr/">YNOV LYON</a>
+        <a href="http://www.ynovlyon.com/fr/">FORMATIONS</a>
+        <a href="http://www.ynovlyon.com/fr/">ENTREPRISES</a>
+        <a href="http://www.ynovlyon.com/fr/actualites/">BLOG</a>
+        <a href="http://www.ynovlyon.com/fr/candidater/" class="nav__menu__canditate">CANDIDATER</a>
+        <a href="http://www.ynovlyon.com/fr/nous-contacter/">CONTACT</a>
+    </div>
+</nav>
+<section class="section">
+    <form method="post" action="">
+        <div class="offre">AJOUTER UNE OFFRE:
+            <input type="number" name="id" placeholder="id">
+            <br/>
+            <input type="text" name="title" placeholder="Titre" required>
+            <br/>
+            <input type="text" name="type" placeholder="Type" required>
+            <br/>
+            <input type="text" name="class" placeholder="Classe" required>
+            <br/>
+            <input type="text" name="period" placeholder="Période" required>
+            <br/>
+            <input type="date" name="from_date" placeholder="Du" required>
+            <br/>
+            <input type="date" name="to_date" placeholder="Au" required>
+            <br/>
+            <input type="text" name="categorie" placeholder="Catégorie" required>
+            <br/>
+            <textarea style="resize: none" name="description" rows="10" cols="50" placeholder="Description de l'offre:"
+                      required>
     </textarea>
-    <br>
-    <input type="submit" value="Envoyer"/>
-    <div>SUPPRIMER UNE OFFRE</div>
-    <?php for ($i = 0; $i < count($bddoffres); $i++){
-        print $bddoffres[$i]['title'] . " " . $bddoffres[$i]['type'] . " " . $bddoffres[$i]['class']  . " " . mb_strimwidth($bddoffres[$i]["description"], 0, 50, "...") . " " . $bddoffres[$i]['period'] . " " . $bddoffres[$i]['from_date'] . " " . $bddoffres[$i]['to_date']. " " . $bddoffres[$i]['categorie'] . " <b>SUPPRIMER</b>" ."<br>";
-    }  ?>
-</form>
+            <br>
+            <input type="submit" value="Envoyer"/>
+        </div>
+        <div>SUPPRIMER UNE OFFRE</div>
+        <?php for ($i = 0; $i < count($bddoffres); $i++) {
+            print $bddoffres[$i]['title'] . " " . $bddoffres[$i]['type'] . " " . $bddoffres[$i]['class'] . " " . mb_strimwidth($bddoffres[$i]["description"], 0, 50, "...") . " " . $bddoffres[$i]['period'] . " " . $bddoffres[$i]['from_date'] . " " . $bddoffres[$i]['to_date'] . " " . $bddoffres[$i]['categorie'] . " <b>SUPPRIMER</b>" . "<br>";
+        } ?>
+    </form>
+</section>
+<img src="/img/footer.png" alt="fouter" class="footer">
 </body>
 </html>
