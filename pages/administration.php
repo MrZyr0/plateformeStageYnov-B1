b75f5e5078099d1e647237c4205dee21a92e9548
Gianni Scantamburlo <gianni.scantamburlo@ynov.com>
2018-06-01 23:49:54 +0200

    supprimer bdd

--- pages/administration.php
+++ pages/administration.php
@@ -1,10 +1,20 @@
 <?php
 require_once($_SESSION["RacineServ"] . '/src/php/lienbdd.php');
 
-$statement = $connection->prepare("
-    INSERT INTO `osi_offer` (`id`, `title`, `type`, `class`, `period`, `from_date`, `to_date`, `categorie`, `description`)
-    VALUES ($_POST[id], $_POST[title], $_POST[type], $_POST[class], $_POST[period], $_POST[from_date], $_POST[to_date], $_POST[categorie], $_POST[description]");
-$statement->execute();
+if (isset($_POST['title'])) {
+    $statement = $connection->prepare('INSERT INTO `osi_offer` (`id`, `title`, `type`, `class`, `description`, `period`, `from_date`, `to_date`, `categorie`) 
+    VALUES (:id, :title, :type, :class, :description, :period, :from_date, :to_date, :categorie)');
+    $statement->bindValue(':id', $_POST["id"]);
+    $statement->bindValue(':title', $_POST["title"]);
+    $statement->bindValue(':type', $_POST["type"]);
+    $statement->bindValue(':class', $_POST["class"]);
+    $statement->bindValue(':description', $_POST["description"]);
+    $statement->bindValue(':period', $_POST["period"]);
+    $statement->bindValue(':from_date', $_POST["from_date"]);
+    $statement->bindValue(':to_date', $_POST["to_date"]);
+    $statement->bindValue(':categorie', $_POST["categorie"]);
+    $statement->execute();
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -14,7 +24,8 @@ $statement->execute();
 </head>
 <body>
 <form method="post" action="">
-    <input type="number" name="id" placeholder="id" >
+    <div>AJOUTER UNE OFFRE:</div>
+    <input type="number" name="id" placeholder="id">
     <br/>
     <input type="text" name="title" placeholder="Titre" required>
     <br/>
@@ -34,6 +45,10 @@ $statement->execute();
     </textarea>
     <br>
     <input type="submit" value="Envoyer"/>
+    <div>SUPPRIMER UNE OFFRE</div>
+    <?php for ($i = 0; $i < count($bddoffres); $i++){
+        print $bddoffres[$i]['title'] . " " . $bddoffres[$i]['type'] . " " . $bddoffres[$i]['class']  . " " . mb_strimwidth($bddoffres[$i]["description"], 0, 50, "...") . " " . $bddoffres[$i]['period'] . " " . $bddoffres[$i]['from_date'] . " " . $bddoffres[$i]['to_date']. " " . $bddoffres[$i]['categorie'] . " <b>SUPPRIMER</b>" ."<br>";
+    }  ?>
 </form>
 </body>
 </html>
